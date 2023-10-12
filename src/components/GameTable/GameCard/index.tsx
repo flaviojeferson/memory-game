fe7d82fc66c4af082eccaa8653ae87da27e7e863
Flávio Jeferson <flaviojeferson.contato@outlook.com>
2023-10-12 17:30:36 -0300

    Incorporate 'card' prop in GameCard

--- src/components/GameTable/GameCard/index.tsx
+++ src/components/GameTable/GameCard/index.tsx
@@ -1,9 +1,17 @@
 import { GameCardContainer } from './styles.ts';
 import questionMarkSvgSrc from '../../../assets/question_mark.svg';
 import { useState } from 'react';
+import { IGameCard } from '../../../@types/Game.ts';
 
-const GameCard: React.FC = () => {
+type GameCardProps = {
+  card: IGameCard;
+};
+
+const GameCard: React.FC<GameCardProps> = ({ card }) => {
+  const { iconSrc } = card;
   const [isVisible, setIsVisible] = useState(false);
+
+  const currentIcon = isVisible ? iconSrc : questionMarkSvgSrc;
 
   const handleToggleVisibility = () => setIsVisible(!isVisible);
   return (
@@ -11,7 +19,7 @@ const GameCard: React.FC = () => {
       className={`card${isVisible ? ' card__visible' : ''}`}
       onClick={handleToggleVisibility}
     >
-      <img className="card__icon" src={questionMarkSvgSrc} alt="" />
+      <img className="card__icon" src={currentIcon} alt="" />
     </GameCardContainer>
   );
 };
--- src/components/GameTable/index.tsx
+++ src/components/GameTable/index.tsx
@@ -1,21 +1,16 @@
+import { shuffleCards } from '../../helpers/shuffleCards.ts';
 import { GameCard } from './GameCard/index.tsx';
 import { GameTableContainer } from './styles.ts';
 
 const GameTable: React.FC = () => {
+  const cards = shuffleCards();
+
+  console.log(cards);
   return (
     <GameTableContainer>
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
+      {cards.map((card) => {
+        return <GameCard card={card} key={card.id} />;
+      })}
     </GameTableContainer>
   );
 };
