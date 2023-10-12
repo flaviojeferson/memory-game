d125ab33de4e31fe77e4527d65fedcdcf02b2a0c
Flávio Jeferson <flaviojeferson.contato@outlook.com>
2023-10-12 17:20:26 -0300

    Add SVG icons and create gameIcons array

---
+++ src/assets/gameIcons.ts
@@ -0,0 +1,17 @@
+import cssSvgSrc from './icons/css.svg';
+import gitSvgSrc from './icons/git.svg';
+import htmlSvgSrc from './icons/html.svg';
+import javascriptSvgSrc from './icons/javascript.svg';
+import npmSvgSrc from './icons/npm.svg';
+import reactSvgSrc from './icons/react.svg';
+
+const gameIcons = [
+  cssSvgSrc,
+  gitSvgSrc,
+  htmlSvgSrc,
+  javascriptSvgSrc,
+  npmSvgSrc,
+  reactSvgSrc,
+];
+
+export { gameIcons };
