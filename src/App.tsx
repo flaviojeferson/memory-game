a8d2cd3aba2986dd6e86a7854fb2816f56961c1d
Flávio Jeferson <flaviojeferson.contato@outlook.com>
2023-10-12 15:05:18 -0300

    Incorporate GameTable in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { defaultTheme } from './styles/themes/defaultTheme';
 import { GameStatus } from './components/GameStatus';
 import { GlobalStyle } from './styles/global';
 import { ThemeProvider } from 'styled-components';
+import { GameTable } from './components/GameTable';
 
 const App: React.FC = () => (
   <>
     <ThemeProvider theme={defaultTheme}>
       <AppContainer>
         <GameStatus />
+        <GameTable />
       </AppContainer>
       <GlobalStyle />
     </ThemeProvider>
--- src/styles/app.ts
+++ src/styles/app.ts
@@ -1,6 +1,10 @@
 import { styled } from 'styled-components';
 
 const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
   min-height: 100vh;
   min-width: 100vw;
   background-color: ${(props) => props.theme['neutral-100']};
